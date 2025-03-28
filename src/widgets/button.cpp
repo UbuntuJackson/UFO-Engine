428326efb9fc93424eb31b0b4945d884cbfadda1
UbuntuJackson <46799187+UbuntuJackson@users.noreply.github.com>
2025-03-28 15:24:26 +0100

    now updating size of buttons during OnUpdate

--- src/widgets/button.cpp
+++ src/widgets/button.cpp
@@ -24,7 +24,7 @@ text{_text}
 void Button::OnUpdate(){
     Widget::OnUpdate();
     if(adjust_height_after_text_rows){
-        float height_of_text_chunk = GetWrappedText(text).rows * 8.0f +6.0f;
+        float height_of_text_chunk = GetWrappedText(text).rows * 8.0f +12.0f;
         if(height_of_text_chunk > GetRectangle().size.y) rectangle.size.y = height_of_text_chunk;
     }
 }
