a7295ac1b42085f704683766681bafc6250eeb7b
UbuntuJackson <46799187+UbuntuJackson@users.noreply.github.com>
2025-04-14 14:49:25 +0200

    ScrollBar

--- src/widgets/scroll_bar.h
+++ src/widgets/scroll_bar.h
@@ -20,13 +20,13 @@ public:
             scroll_percentage = (Mouse::Get().GetPosition()-(GetGlobalPosition()+Vector2f(0.0f,scroll_bar_grab_y))).y / GetRectangle().size.y;
         
         }
-        if(scroll_percentage > 1.0f-visible_portion_percentage) scroll_percentage = 1.0f;
+        if(scroll_percentage > 1.0f - visible_portion_percentage) scroll_percentage = 1.0f - visible_portion_percentage;
         if(scroll_percentage < 0.0f) scroll_percentage = 0.0f;
     }
 
     void OnWidgetDraw(){
         Graphics::Get().DrawRectangle(
-            GetGlobalPosition()+Vector2f(0.0f,scroll_percentage),
+            GetGlobalPosition()+Vector2f(0.0f,scroll_percentage*GetRectangle().size.y),
             Vector2f(16.0f,visible_portion_percentage*GetRectangle().size.y), olc::BLACK);
     }
 
