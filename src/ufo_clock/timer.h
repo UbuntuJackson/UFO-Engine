03272e3486f0d6ef5c938511e483592ce40be5e5
UbuntuJackson <46799187+UbuntuJackson@users.noreply.github.com>
2025-04-16 22:01:26 +0200

    all SpriteReference types now inherit from abstract spritereference

--- src/ufo_clock/timer.h
+++ src/ufo_clock/timer.h
@@ -1,7 +1,6 @@
 #ifndef TIMER
 #define TIMER
 #include <chrono>
-#include "../console/console.h"
 class Timer{
 public:
     std::chrono::time_point<std::chrono::steady_clock> start_time;
@@ -9,35 +8,13 @@ public:
     float time_left_when_paused = 0.0f;
     bool is_started = false;
     bool is_paused = false;
-    Timer(){
-        start_time = std::chrono::steady_clock::now();
-        is_started = false;
-    };
-    void Start(float _number_of_seconds_to_count){
-        number_of_seconds_to_count = _number_of_seconds_to_count;
-        start_time = std::chrono::steady_clock::now();
-        is_started = true;
-    }
-    void Stop(){
-        is_started = false;
-    }
-    float GetTimeLeft(){
-        return number_of_seconds_to_count - std::chrono::duration_cast<std::chrono::milliseconds>((std::chrono::steady_clock::now() - start_time)).count();
-    }
-    void FastForward(float _multply_elapsed_time){
-        if(!is_started) return;
-        float reduced_time = GetTimeLeft() - Engine::Get().GetDeltaTime() * _multply_elapsed_time * 1000.0f;
-        Start(reduced_time);
-    }
-    void Pause(){
-        if(!is_paused) time_left_when_paused = GetTimeLeft();
-        is_paused = true;
-    }
-    void Resume(){
-        //Guard against starting the timer over continuously.
-        if(is_paused) Start(time_left_when_paused);
-        is_paused = false;
-    }
+    Timer();
+    void Start(float _number_of_seconds_to_count);
+    void Stop();
+    float GetTimeLeft();
+    void FastForward(float _multply_elapsed_time);
+    void Pause();
+    void Resume();
 };
 
 #endif
