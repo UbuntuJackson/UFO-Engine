d273c5474dc26be298901cb3205c1f4c7ec86280
UbuntuJackson <46799187+UbuntuJackson@users.noreply.github.com>
2025-04-16 22:57:09 +0200

    all SpriteReference types now inherit from abstract spritereference

--- src/animation/animation.cpp
+++ src/animation/animation.cpp
@@ -22,7 +22,7 @@ void Animation::SetAnimation(std::string _name){
 }
 
 void Animation::OnUpdate(){
-    current_animation_state->position = position;
+    current_animation_state->local_position = local_position;
     frame_counter += Engine::Get().GetDeltaTime() * current_animation_state->pace;
     if(frame_counter >= current_animation_state->number_of_frames) cycle_count++;
     frame_counter = ufoMaths::Wrap(frame_counter, 0.0f, (float)current_animation_state->number_of_frames);
