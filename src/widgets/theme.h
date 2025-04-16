2135ea678f4bce7c5ef486cab9c51627a3f5dc65
UbuntuJackson <46799187+UbuntuJackson@users.noreply.github.com>
2025-04-16 21:57:12 +0200

    all SpriteReference types now inherit from abstract spritereference

--- src/widgets/theme.h
+++ src/widgets/theme.h
@@ -1,11 +1,5 @@
 #pragma once
 
-#include <string>
-#include "../ufo_maths/ufo_maths.h"
-#include "../ufo_engine/ufo_engine.h"
-#include "../external/olcPixelGameEngine.h"
-#include "../graphics/graphics.h"
-
 class Widget;
 
 class Theme{
