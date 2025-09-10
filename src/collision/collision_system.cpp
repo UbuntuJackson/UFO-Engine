7d9b3a1e1b393874fae14ecb6af7b9a2e6ecfb24
UbuntuJackson <46799187+UbuntuJackson@users.noreply.github.com>
2025-09-10 15:09:37 +0200

    Ray2

--- src/collision/collision_system.cpp
+++ src/collision/collision_system.cpp
@@ -484,6 +484,7 @@ void CollisionSystem::Draw(Camera* _camera){
     }
 
     for(const auto& p : polygons){
+        Console::PrintLine("Polygon");
         for(const auto& line : p){
             Graphics::Get().DrawLine(line.Start(), line.End(), olc::YELLOW);
         }
